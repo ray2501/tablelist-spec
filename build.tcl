f7bbedbbf85eade784f028a6bd1affcedcaf6d13
Danilo Chang <ray2501@gmail.com>
2018-06-06 08:43:05 +0800

    Update to 6.2

--- build.tcl
+++ build.tcl
@@ -1,8 +1,8 @@
 #!/usr/bin/tclsh
 
 set arch "noarch"
-set base "tablelist6.1"
-set fileurl "http://www.nemethi.de/tablelist/tablelist6.1.tar.gz"
+set base "tablelist6.2"
+set fileurl "http://www.nemethi.de/tablelist/tablelist6.2.tar.gz"
 
 set var [list wget $fileurl -O $base.tar.gz]
 exec >@stdout 2>@stderr {*}$var
