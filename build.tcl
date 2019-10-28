9d741ebdd28759932022d5581acb09fcfd947b45
Danilo Chang <ray2501@gmail.com>
2019-10-28 11:49:42 +0800

    Update to 6.7

--- build.tcl
+++ build.tcl
@@ -1,7 +1,7 @@
 #!/usr/bin/tclsh
 
 set arch "noarch"
-set base "tablelist6.6"
+set base "tablelist6.7"
 set fileurl "http://www.nemethi.de/tablelist/$base.tar.gz"
 
 set var [list wget $fileurl -O $base.tar.gz]
